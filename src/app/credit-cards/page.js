e83d3291bd0eee76131a1b57ae29ec7e3c418220
kazitishan <kazitishan12@gmail.com>
2026-08-28 03:56:16 -0400

    fixed comments

--- src/app/credit-cards/page.js
+++ src/app/credit-cards/page.js
@@ -6,7 +6,7 @@ export default function CreditCards() {
         <button className="bg-green-800 font-bold text-white p-2 rounded-xl hover:bg-green-900 transition-colors cursor-pointer">Add</button>
       </div>
 
-      {/* All bank accounts */}
+      {/* All credit cards */}
       <div className="flex flex-col gap-4 mt-4">
         
       </div>
--- src/app/subscriptions/page.js
+++ src/app/subscriptions/page.js
@@ -6,7 +6,7 @@ export default function Subscriptions() {
         <button className="bg-green-800 font-bold text-white p-2 rounded-xl hover:bg-green-900 transition-colors cursor-pointer">Add</button>
       </div>
 
-      {/* All bank accounts */}
+      {/* All subscriptions */}
       <div className="flex flex-col gap-4 mt-4">
         
       </div>
